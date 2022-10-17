f7aa7940b033f45f28a7cd813c7a307d3af6fc98
My name <sindanim@gmail.com>
2022-10-17 13:14:37 +0200

    modified:   app.js 	modified:   styles.css

--- app.js
+++ app.js
@@ -19,18 +19,17 @@ const result = document.querySelector(".lorem-text")
 form.addEventListener("submit", function (e) {
   e.preventDefault()
   const value = parseInt(amount.value)
-
   const random = Math.floor(Math.random() * text.length)
   //empty
   //-1
   //>9
-  if (isNaN(value) || value > 0 || value < 9) {
+  if (isNaN(value) || value < 0 || value > 9) {
     result.innerHTML = `<p class = "result">${text[random]}</p>`
   } else {
     let tempText = text.slice(0, value)
     tempText = tempText
       .map(function (item) {
-        return `<p class= "${item}"></p>`
+        return `<p class="result">${item}</p>`
       })
       .join("")
     result.innerHTML = tempText
